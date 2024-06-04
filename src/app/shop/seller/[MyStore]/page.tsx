f733253630a22da0fe07eccf515df48337a0cf67
LeHaiDang <0509dangle@gmail.com>
2024-06-04 13:48:05 +0700

    Add nodat sgv

--- src/app/shop/seller/[MyStore]/page.tsx
+++ src/app/shop/seller/[MyStore]/page.tsx
@@ -14,19 +14,25 @@ function MyStore() {
   return (
     <div className="min-h-screen px-[150px] my-4 grid grid-cols-10 gap-4">
       <div className="flex flex-col bg-white rounded-md p-2 mb-5 col-span-2">
-        {CATEGORYSTORE.map((item: { value: string; title: string }, index) => (
-          <div
-            key={index}
-            className={`${
-              dataCarts.value === item.value
-                ? "bg-blue-300 font-bold text-white"
-                : "bg-blue-100"
-            } cursor-pointer hover:bg-blue-300 transition-all rounded-sm py-3 px-2 mb-2`}
-            onClick={(e) => dispatch(setCategoryStore(item))}
-          >
-            {item.title}
-          </div>
-        ))}
+        {CATEGORYSTORE.map(
+          (item: { value: string; title: string; icon: any }, index) => (
+            <div
+              key={index}
+              className={`${
+                dataCarts.value === item.value
+                  ? "bg-blue-300 font-bold text-white"
+                  : "bg-blue-100"
+              } cursor-pointer hover:bg-blue-300 transition-all rounded-sm py-3 px-2 mb-2`}
+              onClick={(e) => dispatch(setCategoryStore(item))}
+            >
+              <div className="flex items-center">
+                <div className="mr-2">{item.icon}</div>
+
+                {item.title}
+              </div>
+            </div>
+          )
+        )}
       </div>
       <div className=" col-span-8 overflow-scroll scrollbar-hide max-h-screen">
         {dataCarts.value === "home" ? (
