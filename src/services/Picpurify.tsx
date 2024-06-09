927c7f6a2e0f0ed0c69e674480912ae1e47687d2
LeHaiDang <0509dangle@gmail.com>
2024-06-10 01:41:57 +0700

    Test picpirify

---
+++ src/services/Picpurify.tsx
@@ -0,0 +1,11 @@
+import axiosInstance from "@/utils/axiosInterceptor";
+
+// /api/feedback?page=1&limit=1&productId=654745397c3886f34c7ae2c8&userId=1
+// api/feedback-star?productId=654745397c3886f34c7ae2c8
+export const CheckValidImage = async (body: any) => {
+  const res = await axiosInstance.post(
+    process.env.NEXT_PUBLIC_URL_PICPURIFY!,
+    body
+  );
+  return res;
+};
